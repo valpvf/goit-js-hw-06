727b96bf978cb9e27ad3fc4d8e68f2595ae2e6f6
Valerii Pozhydaiev <p_vf@i.ua>
2023-03-15 00:21:29 +0200

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+const itemElArr = [];
+
+for (const ingredient of ingredients) {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  itemElArr.push(itemEl);
+}
+
+listEl.append(...itemElArr);
