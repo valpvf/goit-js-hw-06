c7359a4089fdd245f2a502ad1fd1ae90f89080e2
Valerii Pozhydaiev <p_vf@i.ua>
2023-03-19 16:47:02 +0200

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const refs = {
+  body: document.querySelector("body"),
+  widget: document.querySelector(".widget"),
+  text: document.querySelector(".color"),
+  btn: document.querySelector(".change-color"),
+};
+
+refs.btn.addEventListener("click", onCangeColor);
+
+function onCangeColor(event) {
+  refs.text.textContent = getRandomHexColor();
+  // у разі необхідності виведення кольору тільки на div class="widget"
+  // у наступному рядку треба змінити body на widget
+  refs.body.style.backgroundColor = refs.text.textContent;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
