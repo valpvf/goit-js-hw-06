5320d623c81378ca145de7b58ba3b9ecb23e68e2
Valerii Pozhydaiev <p_vf@i.ua>
2023-03-19 02:49:14 +0200

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+  input: document.querySelectorAll("input"),
+};
 
+refs.form.addEventListener("submit", onFormSubmit);
+refs.input.forEach((el) => el.addEventListener("blur", onInput));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email: login, password },
+  } = event.currentTarget;
+
+  if (login.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const submitData = {
+    login: login.value,
+    password: password.value,
+  };
+  console.log(submitData);
+
+  event.currentTarget.reset();
+}
+
+function onInput(event) {
+  if (event.currentTarget.value.length === 0) {
+    alert("Please fill in all the fields!");
+  }
+}
